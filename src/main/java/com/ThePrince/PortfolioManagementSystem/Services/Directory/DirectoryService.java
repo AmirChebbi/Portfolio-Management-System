558f326bd35061f610f027ebf6b4f40d237e74db
Amir Chebbi <amirchebbi6@gmail.com>
2023-12-30 15:20:18 +0100

    added createNewDirectory method

--- src/main/java/com/ThePrince/PortfolioManagementSystem/Services/Directory/DirectoryService.java
+++ src/main/java/com/ThePrince/PortfolioManagementSystem/Services/Directory/DirectoryService.java
@@ -3,6 +3,7 @@ package com.ThePrince.PortfolioManagementSystem.Services.Directory;
 import com.ThePrince.PortfolioManagementSystem.DAOs.Directory.Directory;
 import com.ThePrince.PortfolioManagementSystem.DAOs.UserEntity.Owner;
 import com.ThePrince.PortfolioManagementSystem.DTOs.Directory.DirectoryDTO;
+import com.ThePrince.PortfolioManagementSystem.DTOs.Directory.DirectoryPathDTO;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,11 @@ public interface DirectoryService {
 
     public Directory intializeGenesisDirectory(Owner owner);
 
+    public DirectoryDTO createNewDirectory(Owner owner, DirectoryPathDTO parentDirectory);
+
     public DirectoryDTO getDirectoryById(long id);
+
+
 
 
 }
